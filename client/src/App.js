bb3d2fbd408d6693c5c5caa0024a12cc5d4f75ea
Ahmed Haddaji <ahmedhaddajiahmed@gmail.com>
2026-03-10 22:47:26 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import LSMI1 from './comps/LSIM1';
 import LSIM2 from './comps/LSIM2';
 import NotFound from './comps/NotFound';
 import DynamicClass from './comps/DynamicClass';
+import Istqb from './comps/Istqb';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/lsim1' element={<LSMI1 />}/>
         <Route path='/lsim2' element={<LSIM2 />}/>
         <Route path="/dynamic-class" element={<DynamicClass />} />
+        <Route path="/istqb" element={<Istqb />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
       
